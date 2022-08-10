87246f51bd4fd2984c389ae3b295b668de58aae9
CyJay96 <konstantin201586@gmail.com>
2022-08-10 12:58:47 +0300

    Initial commit

---
+++ src/main/java/com/ubercadence/UberCadenceApplication.java
@@ -0,0 +1,13 @@
+package com.ubercadence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCadenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberCadenceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ubercadence/UberCadenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ubercadence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCadenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
